5ae864c420f3fb210f0d31a945af57b22c6230bf
hew-2025 <nhs40214@nhs.hal.ac.jp>
2025-02-25 13:27:34 +0900

    Update tikapaqwa.js

--- tikapaqwa.js
+++ tikapaqwa.js
@@ -59,13 +59,6 @@ $(window).on('load', function () {
 	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
 });
 
-// #page-topをクリックした際の設定
-$('#page-top a').click(function () {
-    $('body,html').animate({
-        scrollTop: 0//ページトップまでスクロール
-    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
-    return false;//リンク自体の無効化
-});
 
 
 //アニメーションスタート
