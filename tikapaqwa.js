86de18f0f43e611cf6e212e6643607a876f57757
hew-2025 <nhs40214@nhs.hal.ac.jp>
2025-02-25 15:49:40 +0900

    Update tikapaqwa.js

--- tikapaqwa.js
+++ tikapaqwa.js
@@ -73,40 +73,6 @@ $('.fadeInUpTrigger').on('inview', function(event, isInView) {
     } 
   });// JavaScript Document
 
-$('.slider').slick({
-		autoplay: true,//自動的に動き出すか。初期値はfalse。
-		autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
-		speed:1000,//スライドの動きのスピード。初期値は300。
-		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-		slidesToShow: 1,//スライドを画面に3枚見せる
-		slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-		arrows: true,//左右の矢印あり
-		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-		dots: true,//下部ドットナビゲーションの表示
-        pauseOnFocus: false,//フォーカスで一時停止を無効
-        pauseOnHover: false,//マウスホバーで一時停止を無効
-        pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
-    });
-
-//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
-$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
-    $('.slider').slick('slickPlay');
-});
-
-
-$('.slider').slick({
-		autoplay: true,//自動的に動き出すか。初期値はfalse。
-		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-		speed: 500,//スライドのスピード。初期値は300。
-		slidesToShow: 3,//スライドを画面に3枚見せる
-		slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-		centerMode: true,//要素を中央ぞろえにする
-		variableWidth: true,//幅の違う画像の高さを揃えて表示
-		dots: true,//下部ドットナビゲーションの表示
-	});
 
 let accordionDetails = '.js-details';
 let accordionSummary = '.js-details-summary';
@@ -141,4 +107,18 @@ $(accordionSummary).each(function() {
       $(this).nextAll($(accordionContent)).hide().slideDown(speed);
     }
   })
+});
+
+
+
+$("セレクタ").slick();
+$(".slide-items").slick({
+  autoplay: true, // 自動再生
+});
+$(function () {
+    $(".test-slick").slick({
+        // ~~~
+        centerMode: true,
+        // ~~~
+    });
 });
